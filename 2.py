9c26d74da94e712182124929aa35bcc692755f6b
Sanika Gawande <161837520+sanika-gawande@users.noreply.github.com>
2025-04-28 01:14:27 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+# Add two numbers
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is", sum)
